91a9432c8296b0fd8ba1edd47b2c3997124a3b72
Тимофій Лейцишин <145194518+leitsyshyn@users.noreply.github.com>
2025-08-19 03:04:05 +0300

    fix(page): fix back button styling

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -10,11 +10,10 @@ export default function BackButton({ className }: { className?: string }) {
       type="submit"
       variant="ghost"
       className={cn(
-        "group ease-in duration-80 shadow-none hover:bg-order items-center  w-min pl-2! -translate-x-10",
+        "group relative ease-in duration-80 py-2 hover:bg-transparent pl-10 items-center w-min -translate-x-10",
         className
       )}
     >
-      <Arrow className="size-6" />
       <div className="relative overflow-hidden w-full h-full flex items-center ">
         <span
           className={cn(
@@ -30,7 +29,7 @@ export default function BackButton({ className }: { className?: string }) {
 
         <span
           className={cn(
-            "absolute inset-0 flex items-center  gap-3 transition-[opacity,translate,width] duration-120 ease-out",
+            "absolute inset-0 flex items-center gap-3 transition-[opacity,translate] duration-120 ease-out",
             "group-hover:translate-y-0 group-hover:opacity-100",
             "translate-y-4 opacity-0 "
           )}
@@ -39,6 +38,26 @@ export default function BackButton({ className }: { className?: string }) {
             {t("buttons.back")}
           </span>
         </span>
+      </div>
+      <div
+        className={cn(
+          "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium shrink-0 outline-none absolute top-0 left-0 -z-1 ease-in duration-80 shadow-none group-hover:bg-order w-min p-2 h-11 pr-4"
+        )}
+      >
+        <Arrow className="size-6 shrink-0" />
+        <div className="relative overflow-hidden w-full h-full flex items-center ">
+          <span
+            className={cn(
+              "flex items-center gap-3 transition-[opacity,translate,width] duration-120 ease-out ",
+              "group-hover:translate-y-0 group-hover:opacity-100",
+              "translate-y-4 opacity-0 "
+            )}
+          >
+            <span className="text-lg leading-8 font-semibold opacity-0">
+              {t("buttons.back")}
+            </span>
+          </span>
+        </div>
       </div>
     </Button>
   );
